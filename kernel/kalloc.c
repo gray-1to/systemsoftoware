fcc274b86e86a02180c3723c8c75f422064d405d
gray-1to <graystin88@gmail.com>
2022-10-30 02:46:23 +0900

    init & getppid

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,19 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+// uint64
+// freemem(void){
+//   uint64 free_bite = 0;
+//   struct  run *run;
+
+//   acquire(&kmem.lock);
+//   run = kmem.freelist;
+//   while(run->next){
+//     free_bite += PGSIZE;
+//     run = run->next;
+//   }
+//   release(&kmem.lock);
+
+//   return free_bite;
+// }
